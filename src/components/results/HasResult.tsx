4b55674b0a8e2a6e6a3274abe9205ba0287703e6
MrHencke <hencke1@hotmail.com>
2021-10-21 06:27:20 +0200

    Small changes

--- src/components/results/HasResult.tsx
+++ src/components/results/HasResult.tsx
@@ -1,48 +1,25 @@
-import Resizer from 'react-image-file-resizer';
-import { useEffect, useState } from 'react';
-
+import { ResponsiveBar } from '@nivo/bar';
 const HasResults = ({ state }: any) => {
-	const [resized, setResized] = useState<any>();
 	let response = state[0];
-	let original = state[1];
-	let originalURL = URL.createObjectURL(original);
-	useEffect(() => {
-		resizeFile(original);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	const resizeFile = (file: any) => {
-		try {
-			Resizer.imageFileResizer(file, 28, 28, 'JPEG', 100, 0, (uri) => {
-				setResized(uri);
-			});
-		} catch (err) {
-			console.log(err);
-		}
-	};
+	let file = state[1];
+	let originalURL = URL.createObjectURL(file);
 
 	return (
 		<div>
 			<p>Placeholder for results page</p>
-			{state === undefined ? (
-				<p>There are no results to show</p>
-			) : (
-				<>
-					<p>Your original picture:</p>
-					<img src={originalURL} alt='' style={{ height: '17rem', width: '17rem' }} />
-					<p>This is what our model sees:</p>
-					<img
-						src={resized}
-						alt=''
-						style={{
-							height: '17rem',
-							width: '17rem',
-							filter: 'grayscale() invert()',
-						}}
-					/>
-					<p>We think your picture is of a:</p>
-					{response}
-				</>
-			)}
+
+			<>
+				<p>Your original picture:</p>
+				<img src={originalURL} alt='' style={{ height: '17rem', width: '17rem' }} />
+				<p>This is what our model sees:</p>
+				<img
+					src={response.processed_image}
+					alt=''
+					style={{ height: '17rem', width: '17rem' }}
+				/>
+				<p>We think your picture is of a:</p>
+				{response.top_prediction}
+			</>
 		</div>
 	);
 };
--- src/components/results/Results.tsx
+++ src/components/results/Results.tsx
@@ -1,12 +1,35 @@
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import HasResults from './HasResult';
 
 const Results = () => {
 	const { state }: any = useLocation();
+	const history = useHistory();
+	console.log(state);
+	const [time, setTime] = useState(5);
+
+	useEffect(() => {
+		if (time >= 0 && state === undefined) {
+			const timerID = setInterval(() => tick(), 1000);
+			return () => {
+				clearInterval(timerID);
+			};
+		}
+	});
+
+	const tick = () => {
+		setTime(time - 1);
+	};
+
+	if (time === 0) history.push('/');
+
 	return (
 		<div style={{ textAlign: 'center' }}>
 			{state === undefined ? (
-				<p>There are no results to show</p>
+				<div>
+					<p>There are no results to show</p>
+					<p>Redirecting to home page in: {time} </p>
+				</div>
 			) : (
 				<HasResults state={state} />
 			)}
